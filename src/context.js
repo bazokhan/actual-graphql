54c52cfd9d718ce1ae69886d8ba0ac0133331cd0
bazokhan <albaz2000eg@gmail.com>
2020-01-04 06:11:52 +0200

    add authentication token

--- src/context.js
+++ src/context.js
@@ -1,7 +1,27 @@
 const models = require("../models");
+const jwt = require("jsonwebtoken");
 
-const context = ({ req, res }) => {
-  if (req && req.headers) return { models, ...req.headers };
+const context = async ({ req, res }) => {
+  if (req && req.headers) {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const token = authorization.replace("Bearer ", "");
+      let decoded = null;
+      try {
+        decoded = jwt.verify(token, "112156s4agf3qrewgr$#%@#$");
+      } catch (e) {
+        console.log(e);
+        throw new AuthenticationError("Invalid Token");
+      }
+      if (decoded) {
+        const author = await models.User.findByPk(decoded.id);
+        if(author) {
+          return { models, author, ...req.headers };
+        }
+      }
+    }
+    return { models, ...req.headers };
+  }
   return { models };
 };
 
